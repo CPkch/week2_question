ae9390638f898e10e6beddd2eec96f2a6cb22e4c
CPkch <127062806+CPkch@users.noreply.github.com>
2025-03-24 14:27:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from priority_queue import PriorityQueue
 def main():
-    PQ = PriorityQueue()
+  pq = PriorityQueue()
   
-    PQ.push("Task 1", 3)
-    PQ.push("Task 2", 1)
-    PQ.push("Task 3", 2)
+  pq.push("Task 1", 3)
+  pq.push("Task 2", 1)
+  pq.push("Task 3", 2)
   
-    print(PQ.pop())
-    print(PQ.pop())
-    print(PQ.pop())
+  print(pq.pop())
+  print(pq.pop())
+  print(pq.pop())
 
 if __name__ == "__main__":
-    main()
+  main()
